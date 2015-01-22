4086ee387b27bb16d5db91314eaa33ad62ba35c1
Matt Levinson <matt.a.levinson@gmail.com>
2015-01-22 11:03:02 -0500

    added views

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :users
   resources :movies
 
-  root "movies#index"
+  root "users#index"
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
