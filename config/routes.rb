4aaf1cb4eb94a982f16d61074d8f43132fbabf74
Matt Levinson <matt.a.levinson@gmail.com>
2015-01-22 10:59:41 -0500

    rails setup done

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   resources :users
+  resources :movies
 
-  root "users#index"
+  root "movies#index"
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
