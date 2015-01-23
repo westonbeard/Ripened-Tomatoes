ccfebec8d44c239a399f3839a1585686797c410d
Maury Lindo <maury@cagedcrown.com>
2015-01-23 13:21:10 -0500

    added main css and styling to views

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,35 +1,39 @@
 var apikey = "pq2ed2adddmx9crsnfwvhg36";
+
 var baseUrl = "http://api.rottentomatoes.com";
-var form = $('#search');
-var query = $('#search-name');
-var result = $("#results");
 
-form.on("submit", search);
+// construct the uri with our apikey
+//http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json?apikey=[your_api_key]&page_limit=1
 
-function search(e){
-  e.preventDefault();
+var moviesSearchUrl = baseUrl + '/api/public/v1.0/lists/movies/in_theaters.json?apikey=' + apikey + '&page_limit=20';
 
-  var moviesSearchUrl = baseUrl + '/api/public/v1.0/lists/movies/in_theaters.json?apikey=' + apikey + '&page_limit=8';
+//var query = "Hook";
 
-  console.log(moviesSearchUrl);
-
+$(document).ready(function() {
+ 
+  // send off the query
   $.ajax({
-    url: moviesSearchUrl,
+    url: moviesSearchUrl, //+ '&q=' + encodeURI(query),
     dataType: "jsonp",
+    type: "GET",
     success: searchCallback
   });
-}
-
+});
+ 
+// callback for when we get back the results
 function searchCallback(data) {
+ //$(document.body).append('Found ' + data.total); //+ ' results for ' + query);
+  var movies = data.movies;
 
-  console.log(data);
- result.append('Found ' + data.total + ' results for ' + query);
- var movies = data.movies;
  $.each(movies, function(index, movie) {
+    if ( parseInt(movie.ratings.critics_score) >= 75 ){
+   $(document.body).append('<h1 class="movies">' + movie.title + '</h1>');
+   $(document.body).append('<img src="' + movie.posters.original + '" class="poster" />');
+  $(document.body).append('<div class="score"><h3>' + movie.ratings.critics_score + '%</h3></div>');
 
-    //need to add movies to DB as they populate page
-   result.append('<h1>' + movie.title + '</h1>');
-   result.append('<img src="' + movie.posters.thumbnail + '" />');
+    } else {
+      
+    }
  });
 }
 
